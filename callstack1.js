2441604ba0855da76da682811d0d885a59b2735c
zoy-123 <zoyafaruki242013@gmail.com>
2025-12-02 20:11:59 +0500

    Create callstack1.js

---
+++ callstack1.js
@@ -0,0 +1,19 @@
+<html>
+<body>
+
+<h2>Javacsript arrays</h2>
+<p id="demo"></p>
+
+<script>
+var cars = [
+   "BMW",
+   "Honda Civic",
+   "Ferrari",
+];
+document.getElementById("demo").innerHTML = cars.join("-");
+</script>
+
+</body>
+</html>
+
+
